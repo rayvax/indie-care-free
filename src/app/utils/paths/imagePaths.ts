b7375ee276d3136d09a4ab11fa7e26aa4dbc1973
Rayvax <Rayvax213@gmail.com>
2022-06-30 09:48:56 +0300

    GitHub Pages path refactor

--- src/app/utils/paths/imagePaths.ts
+++ src/app/utils/paths/imagePaths.ts
@@ -1,9 +1,10 @@
 import { getRandomIntInclusive } from "../random-utils";
 
-export const getIconPath = (filename: string) => `/icons/${filename}`;
-export const getImagePath = (filename: string) => `/images/${filename}`;
-export const getAssetImagePath = (index: number) => `/images/assets/${index}.webp`;
+export const getIconPath = (filename: string) => `/indie-care-free/icons/${filename}`;
+export const getImagePath = (filename: string) => `/indie-care-free/images/${filename}`;
+export const getAssetImagePath = (index: number) => `/indie-care-free/images/assets/${index}.webp`;
 export const getRandomAssetImagePath = () => getAssetImagePath(getRandomIntInclusive(1, 19));
-export const getAvatarPath = (avatarIndex: number) => `/images/avatars/other users/${avatarIndex}.webp`;
-export const logoPath = "/images/logo.webp";
-export const mainUserAvatarPath = "/images/avatars/main-user.webp";
+export const getAvatarPath = (avatarIndex: number) => `/indie-care-free/images/avatars/other users/${avatarIndex}.webp`;
+export const logoPath = "/indie-care-free/images/logo.webp";
+export const mainUserAvatarPath = "/indie-care-free/images/avatars/main-user.webp";
+export const categoriesPath = getImagePath('categories');
--- src/app/utils/paths/routerPaths.ts
+++ src/app/utils/paths/routerPaths.ts
@@ -1,9 +1,9 @@
-export const homePagePath = '/';
-export const newbiePagePath = '/new-to-icf';
-export const browseAssetsPagePath = '/assets';
-export const assetPagePath = (id: string) => `/assets/${id}`;
-export const uploadPagePath = '/upload';
-export const loginPagePath = '/login';
-export const registerPagePath = '/register';
-export const profilePagePath = '/profile';
-export const myProfilePagePath = '/profile/my';
+export const homePagePath = '/indie-care-free/';
+export const newbiePagePath = '/indie-care-free/new-to-icf';
+export const browseAssetsPagePath = '/indie-care-free/assets';
+export const assetPagePath = (id: string) => `/indie-care-free/assets/indie-care-free/${id}`;
+export const uploadPagePath = '/indie-care-free/upload';
+export const loginPagePath = '/indie-care-free/login';
+export const registerPagePath = '/indie-care-free/register';
+export const profilePagePath = '/indie-care-free/profile';
+export const myProfilePagePath = '/indie-care-free/profile/indie-care-free/my';
